678e2a71b1df6fc14946f80bcebc8fa788769a63
Anton <levantonio@mail.ru>
2022-10-26 12:58:17 +0300

    lesson 011 find index

---
+++ lesson 011_find_index/Program.cs
@@ -0,0 +1,18 @@
+﻿// ИГДЕКСАЦИЯ  0  1   2   3   4   5   6   7
+int[] array = {1, 25, 36, 47, 58, 69, 85, 258, 47};
+
+int n = array.Length; //количество элементов в массиве array))
+int find = 47;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //если несколько одинаковых значений- укажет первое!!!! и остановит цикл
+    }
+    index += 1;                     
+
+}
