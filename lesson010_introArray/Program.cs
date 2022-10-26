9a3dc934f11346e47d8b8aaa2b25a1fa0b1cf43f
Anton <levantonio@mail.ru>
2022-10-26 12:41:17 +0300

    lesson 010 func

---
+++ lesson010_introArray/Program.cs
@@ -0,0 +1,31 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}
+//             0  1  2  3  4  5  6  7  8 - индексация элементов массива
+int[] array = {17,54,45,69,89,25,20,12,236};
+array[0] = 12;//заменили 17 на 12
+Console.WriteLine(array[0]); //вывод значения по индексу
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);// выводим максимальное число массива(array)
+
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+//int max = Max(
+   // Max(a1, b1, c1),
+   // Max(a2, b2, c2),
+   // Max(a3, b3, c3)); //расписали код выше!!!!!!!!!!!
+
+//Console.WriteLine(max);
