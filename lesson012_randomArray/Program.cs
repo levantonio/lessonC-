c680c679e6ee26525412983f892c14cf85da0909
Anton <levantonio@mail.ru>
2022-10-26 13:35:55 +0300

    lesson 012 random array + void

---
+++ lesson012_randomArray/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index ++;
+    }
+}
+
+
+void PrintArray(int[] col) // void- метод, который ничего не возвращает( нельзя употреблять return)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // типа такой элемент не найден, иначе будет"0"
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index ++;
+    } 
+    return position;    
+}
+
+int[] array = new int[10]; //new int array - создаёт новый массив из 10 элементов по умолчанию "0"
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos); // ищет позицию числа 4
