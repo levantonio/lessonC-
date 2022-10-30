f95ecb6b691ecb8a21d371781602959f425675c4
Anton <levantonio@mail.ru>
2022-10-30 13:45:45 +0300

    lesson 019 фибоначчи

---
+++ lesson019_ fibonacchi/Program.cs
@@ -0,0 +1,13 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+int Fibonacci(int n)
+{
+    if(n==1 || n==2) return 1; //||- это как or в python!!!!!!!!!!!
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++) // i=1-- это из за того, что если будет 0 то будут минусовые значения
+{
+    Console.WriteLine(Fibonacci(i));
+}
