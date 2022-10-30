d5b45798bd03d1f2cae5a15512195dcf78acf4f3
Anton <levantonio@mail.ru>
2022-10-30 13:17:07 +0300

    lesson 018 рекурсия - функция из функции

---
+++ lesson018_Рекурсия/Program.cs
@@ -0,0 +1,8 @@
+﻿int Factorial (int n) // лучше использовать обьем памяти == double!!!
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1); //т.е. 5! = 5*4!
+}
+Console.WriteLine(Factorial(5)); //1*2*3 и др...= 120 
