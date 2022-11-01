5cb0a822e4e7b15bbc46b35eff80d17c23de4da2
Anton <levantonio@mail.ru>
2022-11-01 14:38:03 +0300

    lesson 005

--- lesson005_Random/Program.cs
+++ lesson005_Random/Program.cs
@@ -1,6 +1,6 @@
 ﻿int numberA = new Random().Next(1, 10); //1 2 3 4 5 6....9
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);//new random().Next - генератор случайных чисел в скобках диапазон
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
