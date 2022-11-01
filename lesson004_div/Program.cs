478d28a832ca907bc61d7bc2134dd41172cb5770
Anton <levantonio@mail.ru>
2022-11-01 14:13:00 +0300

    lesson 004

--- lesson004_div/Program.cs
+++ lesson004_div/Program.cs
@@ -1,1 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int numberA = 10;
+int numberB =  5;
+Console.WriteLine(numberA / numberB);//int - целое. double- с точкой! т.е. int 12/5=2 а double 12/5=2.4
