65b91c5bdb9a603509e895db8160693769917ea8
Anton <levantonio@mail.ru>
2022-11-05 04:08:48 +0300

    my project filed

--- my_project/Program.cs
+++ my_project/Program.cs
@@ -1,6 +1,8 @@
 ﻿// написать программу, считающее растояние до следующего км в метрах
 Console.Write("Введите пройденное расстояние в метрах: ");
-int n = Console.ReadLine();
+n = Console.ReadLine();
+int x = 0;
 
+Int32.TryParse(TextBoxD1.Text, out x);
 f = 1000-(n%1000);
 Console.WriteLine(f);
