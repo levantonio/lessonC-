b4a4bf621ef52e8a2b40e5916dfc2c32b09aac79
Anton <levantonio@mail.ru>
2022-11-13 09:45:42 +0300

    all

--- lesson008_while/Program.cs
+++ lesson008_while/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
-int xa = 30, ya = 1,
-xb = 1, yb = 20,
-xc = 50, yc = 20;
+int xa = 40, ya = 1,
+xb = 1, yb = 30,
+xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -21,7 +21,7 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 10000)
+while (count<90000)
 {
     int what = new Random().Next(0, 3); //0, 1 , 2.
     if(what == 0)
